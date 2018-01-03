6be619f30fba348502483cb637524721c9bb0e89
ha3a <harri.heikkila92@gmail.com>
2018-01-03 15:26:26 +0200

    old project

---
+++ src/main/java/tikape/runko/database/MuunnaString.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.runko.database;
+
+
+public class MuunnaString {
+    
+    public String suodataInjektiot(String sana) {
+        String tulos = "";
+        for (int a = 0; a < sana.length(); a++) {
+            if (sana.charAt(a) == '<') {
+                tulos = tulos + '(';
+            } else if (sana.charAt(a) == '>') {
+                tulos = tulos + ')';
+            } else if (sana.charAt(a) == ';') {
+                tulos = tulos + ':';
+            } else if (sana.charAt(a) == '{') {
+                tulos = tulos + '(';
+            } else if (sana.charAt(a) == '}') {
+                tulos = tulos + ')';
+            } else if (sana.charAt(a) == '\'') {    // java käsittelee merkkijonon \' pelkäksi heittomerkiksi
+                tulos = tulos + '"';
+            } else {
+                tulos += sana.charAt(a);
+            }
+        }
+        return tulos;
+    }
+    
+}
